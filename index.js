c493312fbf49232b7251e7fd7592d53cf1005e30
SebastianAlexAndreyR <andreysebastian11.11@gmail.com>
2024-03-14 16:08:57 +0800

    jsfile

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-console.log("Hello World!");
+    console.log("Hello World!");
 
-generateRandomNumber(75);
+    generateRandomNumber(75);
 
-function generateRandomNumber(max) {
-    // Return random number 0 to 10;
-    let randomNum = Math.floor(Math.random() * max) + 1;
-    // if...else if..else statement
-    // if (condition) {block of code to execute}
-    // ==, ! =, >, <, > =, < =, %%, ||
+    function generateRandomNumber(max) {
+        // Return random number 0 to 10;
+        let randomNum = Math.floor(Math.random() * max) + 1;
+        // if...else if..else statement
+        // if (condition) {block of code to execute}
+        // ==, ! =, >, <, > =, < =, %%, ||
 
-    if(randomNum > 15) {
-        console.log('Random Number ${randomNum} is in B. 1 - 15');
-    } else if(randomNum > 16 && randomNum < 30){
-        console.log('Random Number ${randomNum} is in I. 16 - 30');
-    } else if (randomNum > 31 && random < 45) {
-        console.log('Random Number ${randomNum} is in N. 31 - 45');  
-    } else if (randomNum > 46 && random < 51) {
-            console.log('Random Number ${randomNum} is in G. 46 - 51'); 
-    } else if (randomNum > 52 && random < 75) {
-            console.log('Random Number ${randomNum} is in O. 52 - 75');    
-    } else {
-        console.log('Random NUmber is invalid.');
+        if(randomNum <= 15) {
+            return console.log(`Random number ${randomNum} is in B. 1 - 15`);
+        } else if(randomNum >= 16 && randomNum <=30){
+            return console.log(`Random number ${randomNum} is in I. 16 - 30`);
+        } else if (randomNum >= 31 && randomNum <= 45) {
+            return console.log(`Random number ${randomNum} is in N. 31 - 45`);  
+        } else if (randomNum >= 46 && randomNum <= 60) {
+            return  console.log(`Random number ${randomNum} is in G. 46 - 60`); 
+        } else if (randomNum >= 61 && randomNum <= 75) {
+            return  console.log(`Random number ${randomNum} is in O. 61 - 75`);    
+        } else {
+            return console.log(`Random number is invalid.`);
+        }
     }
-
-    return console.log(randomNum);
-
-}
