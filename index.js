3b2cba93e869308acc9ff976187cd4b7738c192f
SebastianAlexAndreyR <andreysebastian11.11@gmail.com>
2024-03-14 15:50:48 +0800

    js file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 console.log("Hello World!");
 
-function generateRandomNumber() {
+generateRandomNumber(75);
+
+function generateRandomNumber(max) {
     // Return random number 0 to 10;
     let randomNum = Math.floor(Math.random() * max) + 1;
     // if...else if..else statement
@@ -8,10 +10,19 @@ function generateRandomNumber() {
     // ==, ! =, >, <, > =, < =, %%, ||
 
     if(randomNum > 15) {
-        console.log('Random Number is in B.');
+        console.log('Random Number ${randomNum} is in B. 1 - 15');
+    } else if(randomNum > 16 && randomNum < 30){
+        console.log('Random Number ${randomNum} is in I. 16 - 30');
+    } else if (randomNum > 31 && random < 45) {
+        console.log('Random Number ${randomNum} is in N. 31 - 45');  
+    } else if (randomNum > 46 && random < 51) {
+            console.log('Random Number ${randomNum} is in G. 46 - 51'); 
+    } else if (randomNum > 52 && random < 75) {
+            console.log('Random Number ${randomNum} is in O. 52 - 75');    
     } else {
-        console.log('Random NUmber is < = to 5');
+        console.log('Random NUmber is invalid.');
     }
 
     return console.log(randomNum);
+
 }
